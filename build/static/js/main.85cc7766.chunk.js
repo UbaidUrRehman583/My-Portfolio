3189c7957bf8c8c6a0c48761c9a2a7ac65e390aa
Ubaid ur Rehman <abbasiubaid80@gmail.com>
2025-07-02 16:07:22 +0500

    init commit

--- build/static/js/main.85cc7766.chunk.js
+++ build/static/js/main.85cc7766.chunk.js
@@ -481,7 +481,7 @@
         u = (a(99), a(3));
       const p = !0,
         g = {
-          title: "Ashutosh's Portfolio",
+          title: "Ubaid's Portfolio",
           description:
             "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
           og: {
